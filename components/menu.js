a2088ea30beade700136f5979b526e8d9ae014b3
ValeriiGri <valeragg65@gmail.com>
2017-11-30 17:22:52 +0300

    done with class

--- components/menu.js
+++ components/menu.js
@@ -1,14 +1,15 @@
 'use strict';
 
-function Menu(el, data = []){
+class Menu{
+  constructor(el, data = []){
         this.el = el;
         this.data = data;
-        
+  }      
           /**
          * Form HTML
          */
-        this.render = function(){
-          el.innerHTML = `
+        render(){
+          this.el.innerHTML = `
             <div class = "menu">
               <span class = "menu__name">menuTitle (click on me)!</span>
               <ul>
@@ -19,23 +20,12 @@ function Menu(el, data = []){
             /**
            * Add items in menu 
            */
-          for(let listItem of data){
+          for(let listItem of this.data){
             
             let liTag = document.createElement('li');
           
             liTag.innerHTML = listItem;
-            el.querySelector('ul').append(liTag);
+            this.el.querySelector('ul').append(liTag);
           }
-        };
-         
-        this.render();
-       
-        let elem = document.querySelector('.menu__name');
-      
-        let showFunc = function(){
-          document.querySelector('ul').classList.toggle('open');
-          document.querySelector('.menu__name').classList.toggle('open');
-        };
-      
-        elem.addEventListener('click', showFunc);
+        }    
 }
